c2d87193b46cd2277bddd2ed570fba7b38540d79
LAUTANAVARRO <87103246+LAUTANAVARRO@users.noreply.github.com>
2024-12-22 20:13:35 -0300

    second commit

---
+++ Desktop/e-commerce/ecommerce-proyect/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav className='navbar'>
+        <a href="#" className='brand'></a>
+
+        <input type='text'/>  
+
+        <div className="links">
+          <a href="#">home</a>
+          <a href="#">about us</a>
+          <a href="#">profile</a>
+          <a href="#">cart</a>
+        </div>
+
+      </nav>
+
+<h1>SPORT SHOP WEBSITE</h1>
+
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+
+    </form>
+
+    </>
+  )
+}
+
+export default App
